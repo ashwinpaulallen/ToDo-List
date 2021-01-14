a57d52803377450230f517aed53283c380738f0f
ashwinpaulallen <ashwin23paul@outlook.com>
2021-01-15 00:42:10 +0530

    added javascript to add new item in list

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const addToDoTemplate = todo => {
+
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-item-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li> `;
+
+    list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+    if(todo.length) {      
+        addToDoTemplate(todo);
+        addForm.reset();
+    }
+});
